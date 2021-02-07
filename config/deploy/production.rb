d1a10651d29b61612db3c37d92078817447b6a39
Sandee Soni <sandeep.soni068@gmail.com>
2021-02-07 18:00:48 +0000

    .

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '13.127.127.146', user: 'ubuntu', password: fetch(:password), roles: %w{web app db}
+server '13.127.127.146', user: 'ubuntu', password: 'Angrybird*16', roles: %w{web app db}
 set :rails_env, 'production'
